53e4ae7118432424ebee133debbb26e62b52935b
endrigobigi <endrigo.bigi@gmx.de>
2020-09-28 11:49:58 +0200

    Service layer, component registration

---
+++ src/main/java/de/endrigobigi/course/services/UserService.java
@@ -0,0 +1,27 @@
+package de.endrigobigi.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.endrigobigi.course.entities.User;
+import de.endrigobigi.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
