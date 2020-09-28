09bee36f93798f1e6cdb0538e431f81d52d1aa26
endrigobigi <endrigo.bigi@gmx.de>
2020-09-28 17:38:53 +0200

    Category

---
+++ src/main/java/de/endrigobigi/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package de.endrigobigi.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.endrigobigi.course.entities.Category;
+import de.endrigobigi.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
