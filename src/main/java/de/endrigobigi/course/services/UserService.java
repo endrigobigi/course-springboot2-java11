8ed4c108824e14ad5242bb0936f2a01ec256ee7a
endrigobigi <endrigo.bigi@gmx.de>
2020-09-29 20:11:29 +0200

    Exception handling - findById

--- src/main/java/de/endrigobigi/course/services/UserService.java
+++ src/main/java/de/endrigobigi/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import de.endrigobigi.course.entities.User;
 import de.endrigobigi.course.repositories.UserRepository;
+import de.endrigobigi.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
