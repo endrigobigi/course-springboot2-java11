b59d5d62b76c64ea3a5a77f6fccb444b9b3543a2
endrigobigi <endrigo.bigi@gmx.de>
2020-09-29 11:29:46 +0200

    Payment, one-to-one association

--- src/main/java/de/endrigobigi/course/config/TestConfig.java
+++ src/main/java/de/endrigobigi/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import de.endrigobigi.course.entities.Category;
 import de.endrigobigi.course.entities.Order;
 import de.endrigobigi.course.entities.OrderItem;
+import de.endrigobigi.course.entities.Payment;
 import de.endrigobigi.course.entities.Product;
 import de.endrigobigi.course.entities.User;
 import de.endrigobigi.course.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1); //no caso de relações um-para-um o valor deve ser salvo na classe independente!
+		orderRepository.save(o1);
 	}
 }
